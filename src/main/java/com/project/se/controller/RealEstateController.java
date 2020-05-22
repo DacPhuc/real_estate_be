b8c0aa620c0eea0b3758093f28e075ea17b3fe5c
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-05-23 02:02:14 +0700

    List all data for table

--- src/main/java/com/project/se/controller/RealEstateController.java
+++ src/main/java/com/project/se/controller/RealEstateController.java
@@ -4,24 +4,30 @@ import com.project.se.domain.Estate;
 import com.project.se.repository.EstateRepository;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 public class RealEstateController {
     @Autowired
     EstateRepository estateRepository;
 
-    @GetMapping("/hello")
-    public ResponseEntity<?> getMethod(){
-        Estate estate = estateRepository.findById(1)
-                .orElse(new Estate());
+    @GetMapping("/estates")
+    public ResponseEntity<?> getMethod(@RequestParam(name = "page") String pageNumber, @RequestParam(name = "pageSize") String pageSize){
+        int page = Integer.parseInt(pageNumber);
+        int size = Integer.parseInt(pageSize);
+        Pageable pagination = PageRequest.of(page, size);
+        Page<Estate> estate = estateRepository.findAll(pagination);
         JSONObject jsonObject = new JSONObject();
-        System.out.println(estate.toString());
         jsonObject.put("result", estate);
         return new ResponseEntity<>(jsonObject, HttpStatus.OK);
     }
--- src/main/java/com/project/se/repository/EstateRepository.java
+++ src/main/java/com/project/se/repository/EstateRepository.java
@@ -1,9 +1,12 @@
 package com.project.se.repository;
 
 import com.project.se.domain.Estate;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
 @Repository
-public interface EstateRepository extends JpaRepository<Estate, Integer> {
+public interface EstateRepository extends PagingAndSortingRepository<Estate, Integer> {
 }
