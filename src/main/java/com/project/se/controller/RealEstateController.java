35b983bb34a4d4ca52b43806f3effd37c13fd9db
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-05-23 15:39:37 +0700

    Handle map view call google api

--- src/main/java/com/project/se/controller/RealEstateController.java
+++ src/main/java/com/project/se/controller/RealEstateController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -30,5 +31,20 @@ public class RealEstateController {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("result", estate);
         return new ResponseEntity<>(jsonObject, HttpStatus.OK);
+    }
+
+    @GetMapping("/estates/geolocation")
+    public ResponseEntity<?> getGeolocation(@RequestParam String id){
+        int estate_id = Integer.parseInt(id);
+        Estate estate = estateRepository.findById(estate_id).orElse(null);
+        RestTemplate restTemplate = new RestTemplate();
+        String street = estate.getAddr_street();
+        String ward = estate.getAddr_ward();
+        String city = estate.getAddr_city();
+        String district = estate.getAddr_district();
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + street + "," + ward + "," + district + "," + city +"&key=AIzaSyCCV-Z0WSK9z5bTLUTZ13s5YVz6I-b2_oE";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(url, String.class);
+        return response;
     }
 }
