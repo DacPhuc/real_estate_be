5312681113d04ed4a41aae9ac22b11c29eaf4f3e
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-05-27 13:03:13 +0700

    Refactor code

--- src/main/java/com/project/se/controller/RealEstateController.java
+++ src/main/java/com/project/se/controller/RealEstateController.java
@@ -2,31 +2,30 @@ package com.project.se.controller;
 
 import com.project.se.domain.Estate;
 import com.project.se.repository.EstateRepository;
+import com.project.se.service.EstateService;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @RestController
 public class RealEstateController {
     @Autowired
     EstateRepository estateRepository;
 
+    @Autowired
+    EstateService estateService;
+
+
     @GetMapping("/estates")
-    public ResponseEntity<?> getMethod(@RequestParam(name = "page") String pageNumber, @RequestParam(name = "pageSize") String pageSize){
-        int page = Integer.parseInt(pageNumber);
-        int size = Integer.parseInt(pageSize);
-        Pageable pagination = PageRequest.of(page, size);
+    public ResponseEntity<?> getMethod(@RequestParam(name = "page") int pageNumber, @RequestParam(name = "pageSize") int pageSize){
+        Pageable pagination = PageRequest.of(pageNumber, pageSize);
         Page<Estate> estate = estateRepository.findAll(pagination);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("result", estate);
@@ -34,17 +33,11 @@ public class RealEstateController {
     }
 
     @GetMapping("/estates/geolocation")
-    public ResponseEntity<?> getGeolocation(@RequestParam String id){
-        int estate_id = Integer.parseInt(id);
-        Estate estate = estateRepository.findById(estate_id).orElse(null);
-        RestTemplate restTemplate = new RestTemplate();
-        String street = estate.getAddr_street();
-        String ward = estate.getAddr_ward();
-        String city = estate.getAddr_city();
-        String district = estate.getAddr_district();
-        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + street + "," + ward + "," + district + "," + city +"&key=AIzaSyCCV-Z0WSK9z5bTLUTZ13s5YVz6I-b2_oE";
-        ResponseEntity<String> response
-                = restTemplate.getForEntity(url, String.class);
-        return response;
+    public ResponseEntity<?> getGeolocation(@RequestParam int id){
+        try {
+            return estateService.getGeolocation(id);
+        }catch (Exception e){
+            return new ResponseEntity<>("Can't find estate have id " + id, HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/project/se/service/EstateService.java
+++ src/main/java/com/project/se/service/EstateService.java
@@ -1,7 +1,29 @@
 package com.project.se.service;
 
+import com.project.se.domain.Estate;
+import com.project.se.repository.EstateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Service
 public class EstateService {
+    @Autowired
+    EstateRepository estateRepository;
+
+    public ResponseEntity getGeolocation(int estateId) throws Exception {
+        Estate estate = estateRepository.findById(estateId).orElseThrow(() -> new Exception("Can find estate have id" + estateId));
+        String street = estate.getAddr_street();
+        String ward = estate.getAddr_ward();
+        String city = estate.getAddr_city();
+        String district = estate.getAddr_district();
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + street + "," + ward + "," + district + "," + city +"&key=AIzaSyCCV-Z0WSK9z5bTLUTZ13s5YVz6I-b2_oE";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> geoLocation
+                = restTemplate.getForEntity(url, String.class);
+        return geoLocation;
+    }
 }
